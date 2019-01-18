7e5a13a0b74dcc0dc41de76eb275465ecddd5676
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-18 19:46:51 +0000

    Deploy EOSIO/auto-static-react-app to github.com/EOSIO/auto-static-react-app.git:gh-pages

---
+++ precache-manifest.f0bd797bae762336f42e7e689a9f9239.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a40bd2f82cff05425816f3a11263026e",
+    "url": "/auto-static-react-app/static/media/stag.a40bd2f8.svg"
+  },
+  {
+    "revision": "a568dd1cf3b01f6679b8",
+    "url": "/auto-static-react-app/static/js/runtime~main.a568dd1c.js"
+  },
+  {
+    "revision": "ae5260c595134d7f6d6f",
+    "url": "/auto-static-react-app/static/js/main.ae5260c5.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/auto-static-react-app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "ae5260c595134d7f6d6f",
+    "url": "/auto-static-react-app/static/css/main.fcef7843.chunk.css"
+  },
+  {
+    "revision": "b3dadf96fbd426284e97301b20abe633",
+    "url": "/auto-static-react-app/index.html"
+  }
+];
